837e944b049bb03b47ad389814e6fd7545a9b6e1
Jesse Vogel <jesse.vogel@tno.nl>
2025-07-22 14:51:09 +0000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install `brew` (https://brew.sh/)
+echo "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install `bat`
+brew install bat
+
+# Install `vim`
+brew install vim
+
+# Install `tmux`
+brew install tmux
